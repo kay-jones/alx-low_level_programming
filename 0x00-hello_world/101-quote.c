6b344fbfe7ef8e1bce1116984199cbb2b31f726f
ugwu kasey <ugwukasey@gmail.com>
2022-09-16 13:05:05 -0700

    A UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(1, "and that peace of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(1,"and that peace of art is useful\"- Dora Korpar, 2015-10-19\n");
 			return (1);
 }
