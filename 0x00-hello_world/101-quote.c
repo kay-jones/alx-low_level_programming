2e708c55160b5f55c76e610659fa5222a211e5d5
ugwu kasey <ugwukasey@gmail.com>
2022-09-16 13:37:28 -0700

    UNIX'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,23 @@
-touch 101-quote.c
 #include <stdio.h>
-#include <unistd.h>
+
+
+
 /**
- *  * main - Entry point
- *   * Return:1
- *    */
+ *
+ * main - program execution starts here
+ *
+ * Description: this is the main function
+ *
+ * Return: an integer to signify success or fail
+ *
+ */
+
 int main(void)
+
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+
+	    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), 59, 1, stderr);
+
+	        return (1);
+
 }
