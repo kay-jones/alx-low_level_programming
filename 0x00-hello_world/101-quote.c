83b8713ae8dc3b56df7e736c3873f74dd08697d1
ugwu kasey <ugwukasey@gmail.com>
2022-09-16 13:25:00 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+touch 101-quote.c
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
- */
+ *  * main - Entry point
+ *   * Return:1
+ *    */
 int main(void)
 {
-	write(1,"and that peace of art is useful\"- Dora Korpar, 2015-10-19\n");
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
