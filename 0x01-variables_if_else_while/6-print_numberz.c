43b88abb80051dc71aeff55f59b0079e952fa01e
ugwu kasey <ugwukasey@gmail.com>
2022-10-14 16:46:21 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - prints out numbers without char  variables
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar("%d", digit);
+		digit++;
+	}
+	putchar("\n");
+	return (0);
+}
