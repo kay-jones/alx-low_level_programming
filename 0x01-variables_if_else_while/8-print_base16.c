1933165cfe35b512e7f8b182dff9232e7d2caa4d
ugwu kasey <ugwukasey@gmail.com>
2022-10-15 16:23:59 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,11 +3,25 @@
 /**
  * main - Entry point
  *
- * Description - base 16
+ * Description - prints all numbers of base 16 in lower case
  *
  * Return: 0.
  */
 int main(void)
 {
+	int digit = 0;
 
+	for (digit < 16)
+	digit++;
+	{
+		if (digit < 10)
+		{
+			putchar(0 + digit);
+		}
+		else if (digit >= 10)
+		{
+			putchar('a' + digit % 10);
+		}
+		putchar('\n');
+		return (0);
 }
