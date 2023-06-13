f6ae28983e4c1dac60f5bae84777c4a2b5498413
ugwu kasey <ugwukasey@gmail.com>
2023-06-13 08:41:40 -0700

    materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Betty stlye */
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar("%d%d", i, j);
+			if (i != 8 || j != 9)
+			{
+				putchar(", ");
+			}
+		}
+		return (0);
+}
